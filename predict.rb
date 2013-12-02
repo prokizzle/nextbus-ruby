18ed7e39421b4604e386fba56564ed09eb67d29f
Nick Prokesch <nprokesch@gmail.com>
2013-12-02 18:10:32 -0500

    cleaned up formatting

--- predict.rb
+++ predict.rb
@@ -1,21 +1,35 @@
 require 'mechanize'
 
 stops = [
-  {agency: "mbta", route: 66, id: 2560},
-  {agency: "mbta", route: 70, id: 1049},
-  {agency: "mbta", route: "70A", id: 1049}
+  {
+    agency: "mbta",
+    route: 66,
+    id: 2560
+  },
+  {
+    agency: "mbta",
+    route: 70,
+    id: 1049
+  },
+  {
+    agency: "mbta",
+    route: "70A",
+    id: 1049
+  },
+  {
+    agency: "mbta",
+    route: "86",
+    id: 1049
+  }
 ]
 
 @a = Mechanize.new
 
 def get_stop_tag(agency, route, keywords)
   routes = @a.get("http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=#{agency}&r=#{route}")
-  # puts routes.xml
   q = routes.xml.to_s.scan(/<stop tag=(.+) title="(.+)" lat.+\/>/)
   g = q.select {|r| !(r[1] =~ /#{keywords}/).nil?}
-  # g.each { |r| puts r[0].gsub('"', '').to_i }
-  g.map!{ |r| [r[0].gsub('"', '').to_i, r[1].to_s] }
-  # w.each {|s| puts s}
+  g.map! { |r| [r[0].gsub('"', '').to_i, r[1].to_s] }
   g.each { |r| puts "tag: #{r[0].to_s}, title: #{r[1]}" }
 
 end
@@ -34,5 +48,4 @@ stops.each do |stop|
 end
 puts "</ul>"
 
-# get_stop_tag("mbta", "70A", "River")
-
+get_stop_tag("mbta", "86", "Everett")
