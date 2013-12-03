63822e9ccf6c5969fff3953f37601ac13b7340b3
Nick Prokesch <nprokesch@gmail.com>
2013-12-02 20:36:46 -0500

    predict method returns hash, added indicator for if bus still running, added support for no more busses, moved first bus/next busses logic to predict method

--- predict.rb
+++ predict.rb
@@ -43,7 +43,14 @@ end
 
 def predict(agency, stopId, route)
   @a.get("http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=#{agency}&stopId=#{stopId}&r=#{route}")
-  return @a.page.xml.to_s.scan(/minutes="(\d+)"/).map{ |r| r.first.to_i }
+  all_busses = @a.page.xml.to_s.scan(/minutes="(\d+)"/).map{ |r| r.first.to_i }
+  unless all_busses.empty?
+    first_bus = all_busses.shift
+    first_bus = "Arriving" if first_bus == 0
+    return {first_bus: first_bus, next_busses: all_busses, running: true}
+  else
+    return {running: false}
+  end
 end
 
 def route_list(agency="mbta")
@@ -57,24 +64,25 @@ def stops_list(route, agency="mbta")
   return @stops
 end
 
-if ARGV.empty?
+
+
+if ARGV[0] == "-s"
+  get_stop_tag("mbta", ARGV[1], ARGV[2])
+else
+  puts "#{ARGV[1]}: #{predict("mbta", ARGV[0], ARGV[1])}" unless ARGV.empty?
   puts "<ul>" if html
   stops.each do |stop|
-    all_busses = predict(stop[:agency], stop[:id], stop[:route])
-    first_bus = all_busses.shift
-    first_bus = "Arriving" if first_bus == 0
-    info = "#{stop[:route]}: \n\tFirst bus: \t#{first_bus} minutes\n\tNext: \t\t#{all_busses}"
-    if html
-      puts "<li>#{info}</li>"
+    result = predict(stop[:agency], stop[:id], stop[:route])
+    if result[:running]
+      info = "#{stop[:route]}: \n\tFirst bus: \t#{result[:first_bus]} minutes\n\tNext: \t\t#{result[:next_busses]}"
+      if html
+        puts "<li>#{info}</li>"
+      else
+        puts info
+      end
     else
-      puts info
+      puts "#{stop[:route]}: \n\tNo busses"
     end
   end
   puts "</ul>" if html
-end
-
-if ARGV[0] == "-s"
-  p route_list
-  p stops_list(86)
-  get_stop_tag("mbta", ARGV[1], ARGV[2])
 end
